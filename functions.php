af01f055f7a3587e4059a329e97b7a6577b7a874
PradecTelecentre <PradecTelecentre>
2020-04-13 22:50:23 +0100

    leadship premier version

--- functions.php
+++ functions.php
@@ -44,7 +44,7 @@ if ( ! function_exists( 'leadship_setup' ) ) :
 
 		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
-			'menu-1' => esc_html__( 'Primary', 'leadship' ),
+			'primary' => esc_html__( 'primary', 'leadship' ),
 		) );
 
 		/*
